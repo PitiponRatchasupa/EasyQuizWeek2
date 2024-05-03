dd71c7dfd02de2aeeec60d4e7a8541fe06182186
PitiponRatchasupa <53926268+PitiponRatchasupa@users.noreply.github.com>
2024-05-03 17:10:58 +0700

    แก้ไข script

--- index.js
+++ index.js
@@ -7,52 +7,55 @@ function checkAnswer1(answer) {
     if (answer === 'a') {
         total++;
     }
-    for(let element of btn){
-        if(element.id != answer){
+    for (let element of btn) {
+        if (element.id != answer) {
             element.style.backgroundColor = 'grey';
         }
         element.disabled = true;
     }
     ans1 = true;
     showResult();
- }
+}
 
- function checkAnswer2(answer) {
+function checkAnswer2(answer) {
     const btn = document.getElementsByClassName('quiz-container2')[0].getElementsByTagName("button");
     if (answer === 'd') {
         total++;
     }
-    for(let element of btn){
-        if(element.id != answer){
+    for (let element of btn) {
+        if (element.id != answer) {
             element.style.backgroundColor = 'grey';
         }
         element.disabled = true;
     }
     ans2 = true;
     showResult();
- }
- 
- function showResult() {
+}
+
+function showResult() {
     const result = document.getElementById('result');
-    if(ans1 && ans2){
+    if (ans1 && ans2) {
         result.textContent = `${total} คะแนน`;
         result.style.color = 'green';
     }
- }
+}
 
- function reload() {
+function reload() {
     const result = document.getElementById('result');
     const btn1 = document.getElementsByClassName('quiz-container1')[0].getElementsByTagName("button");
     const btn2 = document.getElementsByClassName('quiz-container2')[0].getElementsByTagName("button");
-    for(let element of btn1){
+    for (let element of btn1) {
         element.style.backgroundColor = '#007BFF';
         element.disabled = false;
     }
-    for(let element of btn2){
+    for (let element of btn2) {
         element.style.backgroundColor = '#007BFF';
         element.disabled = false;
     }
     result.textContent = ``;
     result.style.color = 'green';
     total = 0;
- }
+    ans2 = false;
+    ans2 = false;
+
+}
